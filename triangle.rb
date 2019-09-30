929dd49bc29c46c536bf7a341bc02d6589c5240c
Scubbero <scubbero.vc@gmail.com>
2019-09-30 13:23:46 -0300

    triangle !

--- triangle.rb
+++ triangle.rb
@@ -14,8 +14,40 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+    validate_triangle a, b, c 
+
+    if a == b && b == c
+        # all equal sides
+        :equilateral
+    elsif a != b && b != c && c != a
+        # all different
+        :scalene
+    else
+        # well... 2 equal
+        :isosceles
+    end
 end
+
+#returns true if the sides make up for a valid triangle
+def validate_triangle(*sides)
+
+    if sides.size != 3
+        raise TriangleError, 'triangles have 3 sides! no more, no less'
+    end
+
+    for side in sides
+        # accounting for 0 or negative side length 
+        if side <= 0
+            raise TriangleError, 'triangle side length must be greather than 0'
+        end
+    end
+
+    sides.sort
+    if sides[0] + sides[1] <= sides[2]
+        raise TriangleError, 'Can\'t form a triangle with those sides'
+    end
+end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
